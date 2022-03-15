3c8813e3d8fd477613c2d1aec7cdddaa9aa1b690
Egor6052 <101550518+Egor6052@users.noreply.github.com>
2022-03-15 20:24:53 +0200

    Logo_Windows98
    Logo_Windows98

---
+++ Logo Windows/src/com/Main.java
@@ -0,0 +1,145 @@
+package com;
+
+public class Main {
+        public static final String ANSI_RESET = "\u001B[0m";
+        public static final String ANSI_BLACK = "\u001B[30m";
+        public static final String ANSI_RED = "\u001B[31m";
+        public static final String ANSI_YELLOW = "\u001B[33m";
+        public static final String ANSI_BLUE = "\u001B[34m";
+        public static final String ANSI_WHITE = "\u001B[37m";
+        public static final String ANSI_GREEN = "\u001B[32m";
+
+        //лого Windows;
+
+
+        public static void main(String[] args) {
+            System.out.println(ANSI_WHITE + "\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588"+ ANSI_RESET);
+
+            System.out.println(ANSI_WHITE + "\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588"+ ANSI_RESET);
+
+            System.out.print(ANSI_WHITE + "\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588"+ ANSI_RESET);
+            System.out.print(ANSI_BLACK+ "\u2588\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.println(ANSI_WHITE+ "\u2588\u2588\u2588\u2588\u2588\u2588" +ANSI_RESET);
+
+            System.out.print(ANSI_WHITE+ "\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLACK+ "\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.println(ANSI_WHITE+ "\u2588\u2588\u2588\u2588" +ANSI_RESET);
+
+            System.out.print(ANSI_WHITE+ "\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLACK+ "\u2588" +ANSI_RESET);
+            System.out.print(ANSI_WHITE+ "\u2588\u2588\u2588\u2588\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLACK+ "\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_RED+ "\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLACK+ "\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_GREEN+ "\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLACK+ "\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.println(ANSI_WHITE+ "\u2588" +ANSI_RESET);
+
+            System.out.print(ANSI_WHITE+ "\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLACK+ "\u2588" +ANSI_RESET);
+            System.out.print(ANSI_WHITE+ "\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLACK+ "\u2588" +ANSI_RESET);
+            System.out.print(ANSI_WHITE+ "\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLACK+ "\u2588\u2588\u2588\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_RED+ "\u2588\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLACK+ "\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_GREEN+ "\u2588\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLACK+ "\u2588\u2588" +ANSI_RESET);
+            System.out.println(ANSI_WHITE+ "\u2588" +ANSI_RESET);
+
+            System.out.print(ANSI_WHITE+ "\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLACK+ "\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_RED+ "\u2588\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLACK+ "\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_GREEN+ "\u2588\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLACK+ "\u2588\u2588" +ANSI_RESET);
+            System.out.println(ANSI_WHITE+"\u2588" +ANSI_RESET);
+
+            System.out.print(ANSI_WHITE+ "\u2588" +ANSI_RESET);
+            System.out.print(ANSI_RED+ "\u2588" +ANSI_RESET);
+            System.out.print(ANSI_WHITE+ "\u2588\u2588\u2588\u2588\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLACK+ "\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_RED+ "\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLACK+ "\u2588\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_GREEN+ "\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLACK+ "\u2588\u2588" +ANSI_RESET);
+            System.out.println(ANSI_WHITE+"\u2588" +ANSI_RESET);
+
+            System.out.print(ANSI_WHITE+"\u2588" +ANSI_RESET);
+            System.out.print(ANSI_RED+ "\u2588" +ANSI_RESET);
+            System.out.print(ANSI_WHITE+"\u2588" +ANSI_RESET);
+            System.out.print(ANSI_RED+ "\u2588" +ANSI_RESET);
+            System.out.print(ANSI_WHITE+"\u2588" +ANSI_RESET);
+            System.out.print(ANSI_RED+ "\u2588\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLACK+ "\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.println(ANSI_WHITE+"\u2588" +ANSI_RESET);
+
+            System.out.print(ANSI_WHITE+"\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_RED+ "\u2588\u2588\u2588\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLACK+ "\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLUE+"\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLACK+ "\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_YELLOW+"\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLACK+ "\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.println(ANSI_WHITE+"\u2588" +ANSI_RESET);
+
+            System.out.print(ANSI_WHITE+"\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLUE+"\u2588" +ANSI_RESET);
+            System.out.print(ANSI_WHITE+"\u2588\u2588\u2588\u2588\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLACK+ "\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLUE+"\u2588\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLACK+ "\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_YELLOW+"\u2588\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLACK+ "\u2588\u2588" +ANSI_RESET);
+            System.out.println(ANSI_WHITE+"\u2588" +ANSI_RESET);
+
+            System.out.print(ANSI_WHITE+"\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLUE+"\u2588" +ANSI_RESET);
+            System.out.print(ANSI_WHITE+"\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLUE+"\u2588" +ANSI_RESET);
+            System.out.print(ANSI_WHITE+"\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLUE+"\u2588\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLACK+ "\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLUE+"\u2588\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLACK+ "\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_YELLOW+"\u2588\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLACK+ "\u2588\u2588" +ANSI_RESET);
+            System.out.println(ANSI_WHITE+"\u2588" +ANSI_RESET);
+
+            System.out.print(ANSI_WHITE+"\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLUE+"\u2588\u2588\u2588\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLACK+ "\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLUE+"\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLACK+ "\u2588\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_YELLOW+"\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLACK+ "\u2588\u2588" +ANSI_RESET);
+            System.out.println(ANSI_WHITE+"\u2588" +ANSI_RESET);
+
+            System.out.print(ANSI_WHITE+"\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLACK+ "\u2588" +ANSI_RESET);
+            System.out.print(ANSI_WHITE+"\u2588" +ANSI_RESET);
+            System.out.print(ANSI_WHITE+"\u2588\u2588\u2588\u2588\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLACK+ "\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.println(ANSI_WHITE+"\u2588\u2588" +ANSI_RESET);
+
+            System.out.print(ANSI_WHITE+"\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLACK+ "\u2588" +ANSI_RESET);
+            System.out.print(ANSI_WHITE+"\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLACK+ "\u2588" +ANSI_RESET);
+            System.out.print(ANSI_WHITE+"\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLACK+ "\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_WHITE+ "\u2588\u2588\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLACK+ "\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.println(ANSI_WHITE+"\u2588\u2588\u2588" +ANSI_RESET);
+
+
+            System.out.print(ANSI_WHITE+"\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLACK+ "\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_WHITE+"\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588" +ANSI_RESET);
+            System.out.print(ANSI_BLACK+ "\u2588" +ANSI_RESET);
+            System.out.println(ANSI_WHITE+"\u2588\u2588\u2588" +ANSI_RESET);
+
+        }
+
+    }
+
